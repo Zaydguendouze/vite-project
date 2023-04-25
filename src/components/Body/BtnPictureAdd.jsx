ea2296a6815e36e66ccede4dd69521cec6c94a1a
Zaydguendouze <z.guendouze@outlook.fr>
2023-04-25 18:39:43 +0200

    rendu css images

---
+++ src/components/Body/BtnPictureAdd.jsx
@@ -0,0 +1,9 @@
+export default function BtnPictureAdd() {
+  return (
+    <div>
+      <button className="mt-5 mb-5 bg-blue-500 m-1 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Ajouter une image
+      </button>
+    </div>
+  );
+}
--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,9 +1,15 @@
 import Images from "../Pictures";
+import BtnPictureAdd from "./BtnPictureAdd";
 
 export default function Body() {
   return (
-    <div className="flex justify-between">
-      <Images />
+    <div className="flex flex-wrap justify-center items-center">
+      <div className="flex flex-wrap justify-center">
+        <Images />
+      </div>
+      <div className="mt-4">
+        <BtnPictureAdd />
+      </div>
     </div>
   );
 }
